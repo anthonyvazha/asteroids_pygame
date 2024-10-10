d2b11a795fe0354a4941e75deddd11a54b24b0ca
Anthony Vazhapilly <anthonyvazha@gmail.com>
2024-10-09 19:39:18 -0500

    Added Arrow Pointer for Player

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import pygame
 from constants import *
-
-
+from player import Player
 
 
 def main():
-    print('Starting asteroids!')
-    pygame.get_init()
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        screen.fill('black')
+
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        # limit the frame rate to 60 fps
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 
